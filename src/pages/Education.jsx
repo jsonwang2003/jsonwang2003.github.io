d112397211c71a0ca3faaee63d778f4c384de46f
jsonwang2003 <jason0100w@gmail.com>
2025-07-17 14:51:03 -0700

    defualt format for pages

--- src/pages/Education.jsx
+++ src/pages/Education.jsx
@@ -1,9 +1,33 @@
+import { Container, Box, Typography, Divider } from '@mui/material';
+import { useTheme } from '@mui/material/styles';
+
 function Education() {
+   const theme = useTheme();
    return (
-      <div className="education">
-         <h1>Education</h1>
-         <p>Details about my education will go here.</p>
-      </div>
+      <Container
+         component="div"
+         sx={{
+            width: '100%', 
+            padding: 2, 
+            boxSizing: 'border-box',
+         }}
+      >
+         <Box>
+            <Typography
+               variant="h1"
+               sx={{
+                  marginBottom: 2,
+                  color: theme.palette.text.primary,
+                  fontSize: '3rem',
+                  fontFamily: 'sans-serif',
+                  ml: 5
+               }}
+            >
+               Education
+            </Typography>
+            <Divider sx={{ backgroundColor: theme.palette.text.primary, marginBottom: 2 }}/>
+         </Box>
+      </Container>
    );
 }
 
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,9 +1,33 @@
+import { Container, Box, Typography, Divider } from '@mui/material';
+import { useTheme } from '@mui/material/styles';
+
 function Projects() {
+   const theme = useTheme();
    return (
-      <div className="projects">
-         <h1>Projects</h1>
-         <p>Details about my projects will go here.</p>
-      </div>
+      <Container
+         component="div"
+         sx={{
+            width: '100%', 
+            padding: 2, 
+            boxSizing: 'border-box',
+         }}
+      >
+         <Box>
+            <Typography
+               variant="h1"
+               sx={{
+                  marginBottom: 2,
+                  color: theme.palette.text.primary,
+                  fontSize: '3rem',
+                  fontFamily: 'sans-serif',
+                  ml: 5
+               }}
+            >
+               Projects
+            </Typography>
+            <Divider sx={{ backgroundColor: theme.palette.text.primary, marginBottom: 2 }}/>
+         </Box>
+      </Container>
    );
 }
 
